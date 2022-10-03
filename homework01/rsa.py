cf85e0a412c258c03404356b6e17db5207ec4383
LevaInformatik <111689867+LevaInformatik@users.noreply.github.com>
2022-10-03 22:43:18 +0300

    ttttt

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,5 +1,7 @@
 import random
 import typing as tp
+
+
 def is_prime(n: int) -> bool:
     if n == 1:
         return False
@@ -7,6 +9,8 @@ def is_prime(n: int) -> bool:
         if n % i == 0:
             return False
     return True
+
+
 def gcd(a: int, b: int) -> int:
     if a == 0 or b == 0:
         return max(a, b)
@@ -15,15 +19,19 @@ def gcd(a: int, b: int) -> int:
         if a % i == 0 and b % i == 0 and i > max_del:
             max_del = i
     return max_del
+
+
 def multiplicative_inverse(e: int, phi: int) -> int:
     return pow(e, -1, phi)
+
+
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:
         raise ValueError("p and q cannot be equal")
     n = p * q
-    phi = (p-1) * (q-1)
+    phi = (p - 1) * (q - 1)
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
     # Use Euclid's Algorithm to verify that e and phi(n) are coprime
@@ -36,6 +44,8 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     # Return public and private keypair
     # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
+
+
 def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
     # Unpack the key into it's components
     key, n = pk
@@ -44,6 +54,8 @@ def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
     cipher = [(ord(char) ** key) % n for char in plaintext]
     # Return the array of bytes
     return cipher
+
+
 def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     # Unpack the key into its components
     key, n = pk
@@ -51,6 +63,8 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     plain = [chr((char**key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
+
+
 if __name__ == "__main__":
     print("RSA Encrypter/ Decrypter")
     p = int(input("Enter a prime number (17, 19, 23, etc): "))
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -2,62 +2,63 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     ciphertext = ""
 
     for i in range(len(plaintext)):
-      l = ord(plaintext[i])
-      shift = ord(keyword[i % len(keyword)])
+        l = ord(plaintext[i])
+        shift = ord(keyword[i % len(keyword)])
 
-      k = ord(keyword[i % len(keyword)])
-      if k >= ord('a') and k <= ord('z'):
-        shift -= ord('a')
-      else:
-        shift -= ord('A')
-
-      if plaintext[i].isalpha():
-        if l >= ord('a') and l <= ord('z'):
-          temp = l + shift
-          if temp > ord('z'):
-            ciphertext += chr(shift - (ord("z") - l) + ord("a") - 1)
-          else:
-            ciphertext += chr(temp)
+        k = ord(keyword[i % len(keyword)])
+        if k >= ord("a") and k <= ord("z"):
+            shift -= ord("a")
         else:
-          temp = l + shift
-          if temp > ord('Z'):
-            ciphertext += chr(shift - (ord("Z") - l) + ord("A") - 1)
-          else:
-            ciphertext += chr(temp)
-      else:
-        ciphertext += plaintext[i]
+            shift -= ord("A")
+
+        if plaintext[i].isalpha():
+            if l >= ord("a") and l <= ord("z"):
+                temp = l + shift
+                if temp > ord("z"):
+                    ciphertext += chr(shift - (ord("z") - l) + ord("a") - 1)
+                else:
+                    ciphertext += chr(temp)
+            else:
+                temp = l + shift
+                if temp > ord("Z"):
+                    ciphertext += chr(shift - (ord("Z") - l) + ord("A") - 1)
+                else:
+                    ciphertext += chr(temp)
+        else:
+            ciphertext += plaintext[i]
 
     return ciphertext
+
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     ciphertexttext = ""
 
     for i in range(len(ciphertext)):
-      l = ord(ciphertext[i])
-      shift = ord(keyword[i % len(keyword)])
+        l = ord(ciphertext[i])
+        shift = ord(keyword[i % len(keyword)])
 
-      k = ord(keyword[i % len(keyword)])
-      if k >= ord('a') and k <= ord('z'):
-        shift -= ord('a')
-      else:
-        shift -= ord('A')
-
-      shift = 26 - shift
-
-      if ciphertext[i].isalpha():
-        if l >= ord('a') and l <= ord('z'):
-          temp = l + shift
-          if temp > ord('z'):
-            ciphertexttext += chr(shift - (ord("z") - l) + ord("a") - 1)
-          else:
-            ciphertexttext += chr(temp)
+        k = ord(keyword[i % len(keyword)])
+        if k >= ord("a") and k <= ord("z"):
+            shift -= ord("a")
         else:
-          temp = l + shift
-          if temp > ord('Z'):
-            ciphertexttext += chr(shift - (ord("Z") - l) + ord("A") - 1)
-          else:
-            ciphertexttext += chr(temp)
-      else:
-        ciphertexttext += ciphertext[i]
+            shift -= ord("A")
+
+        shift = 26 - shift
+
+        if ciphertext[i].isalpha():
+            if l >= ord("a") and l <= ord("z"):
+                temp = l + shift
+                if temp > ord("z"):
+                    ciphertexttext += chr(shift - (ord("z") - l) + ord("a") - 1)
+                else:
+                    ciphertexttext += chr(temp)
+            else:
+                temp = l + shift
+                if temp > ord("Z"):
+                    ciphertexttext += chr(shift - (ord("Z") - l) + ord("A") - 1)
+                else:
+                    ciphertexttext += chr(temp)
+        else:
+            ciphertexttext += ciphertext[i]
 
     return ciphertexttext
